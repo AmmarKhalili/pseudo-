e1a8f614d992eed733dacacbdb15ceed6d5ae0af
AmmarKhalili <132856528+AmmarKhalili@users.noreply.github.com>
2023-12-23 21:19:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// G-Version - Split the Nota
+// Skriv din pseudokod innanför nedanstående kommentarsblock
+/*
+function  splitNota(Summa, antalvänner, dricks )
+    SET variabel dricsBelopp = Summa . dricks
+    SET variabel totaltBelopp= Summa + dricsBelopp 
+    SET variabel beloppPerVän= totaltBelopp / antalvänner 
+
+ end function
+
+ Print"Summan"
+ input"Summa"
+ Print"Vänner"
+ input"antalvänner"
+ Print"Dricks"
+ input"dricks"
+
+ Print("Värje person ska betala" + beloppPerVän)
+
+
+*/
+
+// VG-Version - Lewis Carroll Word Puzzle
+// Skriv din pseudokod innanför nedanstående kommentarsblock
+/*
+
+function play(ordbok, startord, slutord)
+    SET variabel ordbok = [..., ..., ...]; // Innehåller ALLA ord i det engelska språket
+    SET variabel startOrd till "FOUR";
+    SET variabel slutordOrd till "FIVE";
+
+
+
+
+end function
+
+function isOneLetterApart(wordOne, wordTwo)
+    SET variabel diffCount till 0;
+
+
+    return diffCount === 1; // returnerar sant om endast en bokstav ändrats, annars falskt
+end function
+
+*/
